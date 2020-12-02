f3a27f8e381f216aa30e8b8206c0ac6b1e721229
Fabio Mazza <fab4mazz@gmail.com>
2020-12-03 00:36:18 +0100

    Make it all work again with python 3.9

--- gtfs_realtime_json.py
+++ gtfs_realtime_json.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-import sys, urllib2
-import gtfs_realtime_pb2, nyct_subway_pb2, protobuf_json
+import sys, urllib.request, urllib.error, urllib.parse
+import gtfs_realtime_pb2
+import protobuf_json
 import json
 
 def main():
     gtfs_realtime = gtfs_realtime_pb2.FeedMessage()
-    gtfs_realtime.ParseFromString(urllib2.urlopen(sys.argv[1]).read())
-    print json.dumps(protobuf_json.pb2json(gtfs_realtime), separators=(',',':'))
+    req = urllib.request.urlopen(sys.argv[1])
+    data = req.read()
+    gtfs_realtime.ParseFromString(data)
+    print(json.dumps(
+        protobuf_json.pb2json(gtfs_realtime), separators=(',',':'), indent=2))
     return
     
 if __name__ == '__main__':
--- json_gtfs_realtime.py
+++ json_gtfs_realtime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from google.protobuf import json_format
-import sys, urllib2
+import sys, urllib.request, urllib.error, urllib.parse
 import gtfs_realtime_pb2
 import json
 
 def main():
-    json_rt = json.load(urllib2.urlopen(sys.argv[1]))
+    json_rt = json.load(urllib.request.urlopen(sys.argv[1]))
     gtfs_realtime = gtfs_realtime_pb2.FeedMessage()
     json_format.ParseDict(json_rt, gtfs_realtime)
     with open(sys.argv[2], 'wb') as f:
